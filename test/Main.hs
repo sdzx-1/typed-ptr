2269f6a308481e93a76b622a3c574c95bfcfce60
sdzx-1 <shangdizhixia1993@163.com>
2024-10-08 12:43:12 +0800

    Improve test

--- test/Main.hs
+++ test/Main.hs
@@ -1,21 +1,33 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE NumericUnderscores #-}
+{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE QualifiedDo #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+{-# OPTIONS_GHC -Wno-unused-top-binds #-}
 
 module Main (main) where
 
-import Control.Monad (replicateM_)
+import Control.Concurrent (threadDelay)
 import Data.IFunctor (At (..))
 import qualified Data.IFunctor as I
 import Data.Type.Map (type (:->) ((:->)))
+import Foreign
+import Foreign.C hiding (CUChar, CULong, CUShort)
+import GHC.IO.Device (IODeviceType (..))
+import GHC.IO.FD (FD (..), openFile, release)
+import GHC.IO.IOMode (IOMode (..))
 import TypedPtr
 
 main :: IO ()
 main = do
-  putStrLn "Test suite not yet implemented."
-  replicateM_ 100 $ runMPtr foo
+  -- replicateM_ 100 $ runMPtr foo
+  foo1
 
 type K1Struct =
   '[ "field0" ':-> Bool
@@ -77,3 +89,149 @@ foo = I.do
   freeptr k1
   freeptr k2
   freeptr k3
+
+type U32 = Word32
+type U8 = Word8
+type CUint = U32
+
+type CUChar = U8
+type TcFlag_t = CUint
+type CC_t = CUChar
+type Speed_t = CUint
+type CUShort = Word16
+type CULong = Word64
+type Ioctl = CULong
+
+type TermSize =
+  '[ "row" ':-> CUShort
+   , "col" ':-> CUShort
+   , "x" ':-> CUShort
+   , "y" ':-> CUShort
+   ]
+
+type Termios =
+  '[ "c_iflag" ':-> TcFlag_t
+   , "c_oflag" ':-> TcFlag_t
+   , "c_cflag" ':-> TcFlag_t
+   , "c_lflag" ':-> TcFlag_t
+   , "c_line" ':-> CC_t
+   , "c_cc" ':-> Array 32 CC_t
+   , "c_ispeed" ':-> Speed_t
+   , "c_ospeed" ':-> Speed_t
+   ]
+
+defaultTermSize :: Struct (CollVal TermSize)
+defaultTermSize =
+  80
+    :& 60
+    :& 0
+    :& 0
+    :& End
+
+defaultTermios :: Struct (CollVal Termios)
+defaultTermios =
+  0
+    :& 0
+    :& 0
+    :& 0
+    :& 0
+    :& (ArrayC [0 .. 31])
+    :& 0
+    :& 0
+    :& End
+
+pattern TIOCGWINSZ :: Ioctl
+pattern TIOCGWINSZ = 0x5413
+
+foreign import ccall unsafe "ioctl"
+  c_ioctl
+    :: CInt
+    -> Ioctl
+    -> Ptr (Struct (CollVal TermSize))
+    -> IO CInt
+
+showIOT :: IODeviceType -> String
+showIOT = \case
+  Directory -> "Directory"
+  Stream -> "Stream"
+  RegularFile -> "RegularFile"
+  RawDevice -> "RawDevice"
+
+foreign import ccall unsafe "isatty"
+  c_isatty :: CInt -> IO CInt
+
+foreign import ccall unsafe "ttyname"
+  c_ttyname :: CInt -> IO CString
+
+foreign import ccall unsafe "tcgetattr"
+  c_tcgetattr
+    :: CInt
+    -> (Ptr (Struct (CollVal Termios)))
+    -> IO Int
+
+pattern TCSANOW :: CInt
+pattern TCSANOW = 0
+
+foreign import ccall unsafe "tcsetattr"
+  c_tcsetattr
+    :: CInt
+    -> CInt
+    -> (Ptr (Struct (CollVal Termios)))
+    -> IO Int
+
+tcsetattr
+  :: CInt
+  -> Ptr (Struct (CollVal Termios))
+  -> IO Int
+tcsetattr fd ptr =
+  c_tcsetattr fd TCSANOW ptr
+
+tcgetattr :: FD -> Ptr (Struct (CollVal Termios)) -> IO Bool
+tcgetattr FD{fdFD} ptr = do
+  val <- c_tcgetattr fdFD ptr
+  pure $ case val of
+    0 -> True
+    _ -> False
+
+ttyname :: FD -> IO String
+ttyname FD{fdFD} = do
+  ptr <- c_ttyname fdFD
+  peekCString ptr
+
+isatty :: FD -> IO Bool
+isatty FD{fdFD} = do
+  val <- c_isatty fdFD
+  pure $ case val of
+    0 -> False
+    _ -> True
+
+foo1 :: IO ()
+foo1 = do
+  (fd@FD{fdFD}, iot) <-
+    openFile
+      "/dev/tty"
+      ReadWriteMode
+      False
+  print fd
+  putStrLn $ showIOT iot
+  checkVal <- isatty fd
+  print checkVal
+  name <- ttyname fd
+  print name
+  ptr <- malloc @(Struct (CollVal Termios))
+  ptr1 <- malloc @(Struct (CollVal TermSize))
+  peek ptr >>= print
+  poke ptr defaultTermios
+  peek ptr >>= print
+  val <- tcgetattr fd ptr
+  print val
+  peek ptr >>= print
+  v1 <- c_ioctl fdFD TIOCGWINSZ ptr1
+  print ("TermSizeResult", v1)
+  peek ptr1 >>= print
+
+  threadDelay 3_000_000
+  free ptr
+  free ptr1
+  release fd
+  print "finish"
